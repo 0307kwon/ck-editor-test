af7f3d063c8b525017835219f2f807508c646a0a
0307kwon <0307kwon@gmail.com>
2022-12-28 22:57:13 +0900

    chore: init ck editor framework environment

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading";
+import List from "@ckeditor/ckeditor5-list/src/list";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+
+ClassicEditor.create(document.querySelector("#editor"), {
+  plugins: [Essentials, Paragraph, Heading, List, Bold, Italic],
+  toolbar: ["heading", "bold", "italic", "numberedList", "bulletedList"],
+})
+  .then((editor) => {
+    console.log("Editor was initialized", editor);
+  })
+  .catch((error) => {
+    console.error(error.stack);
+  });
